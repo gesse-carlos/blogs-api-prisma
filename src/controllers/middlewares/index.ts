f7fc235f6c4fbd175eb5b3728f6619331c9d88a3
Gessé Carlos <gesse.dev@outlook.com>
2022-02-06 15:11:59 -0300

    create index.ts

---
+++ src/controllers/middlewares/index.ts
@@ -0,0 +1,5 @@
+export * as userMiddleware from './user';
+export * as categoryMiddleware from './category';
+export * as postMiddleware from './post';
+export * as loginMiddleware from './login';
+export * as jwtMiddleware from './jwt';
